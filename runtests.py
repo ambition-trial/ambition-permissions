c3eadd0794cc6cd951601308970725e73679cbd8
erikvw <ew2789@gmail.com>
2019-04-04 09:08:32 -0500

    setup

--- runtests.py
+++ runtests.py
@@ -60,6 +60,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "edc_reference.apps.AppConfig",
         "edc_permissions.apps.AppConfig",
         'ambition_ae.apps.AppConfig',
+        'ambition_auth.apps.AppConfig',
         'ambition_lists.apps.AppConfig',
         'ambition_prn.apps.AppConfig',
         'ambition_rando.apps.AppConfig',
